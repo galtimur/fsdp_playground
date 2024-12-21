b50b04c4e339e6a97a8b0efd8f5f1202b5ddd493
galtimur <gal_timur@yahoo.com>
2024-12-21 10:52:55 +0100

    llama-1B training is runnable

--- fsdp_playground.py
+++ fsdp_playground.py
@@ -1,51 +1,25 @@
-#%%
+# %%
 # Based on: https://github.com/pytorch/examples/blob/master/mnist/main.py
 import os
-import functools
+
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from tqdm import tqdm
-import datetime
-
-
-from torch.optim.lr_scheduler import StepLR
-
 import torch.distributed as dist
 import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data.distributed import DistributedSampler
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-
-from torch.distributed.fsdp import MixedPrecision, ShardingStrategy
-from torch.distributed.fsdp.fully_sharded_data_parallel import (
-    CPUOffload,
-    BackwardPrefetch,
-)
-from torch.distributed.fsdp.wrap import (
-    size_based_auto_wrap_policy,
-    enable_wrap,
-    wrap,
-)
-
-from omegaconf import OmegaConf
-import functools
-
 from datasets import load_dataset
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    default_data_collator,
-)
+from omegaconf import OmegaConf
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.utils.data.distributed import DistributedSampler
+from tqdm import tqdm
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          default_data_collator)
 
-#%% md
+
+# %% md
 # 1.3 Distributed training setup. As we mentioned FSDP is a type of data parallelism which requires a distributed training environment, so here we use two helper functions to initialize the processes for distributed training and clean up.
-#%%
+# %%
 def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
 
     # os.environ["NCCL_DEBUG"] = "INFO"
     # os.environ["NCCL_SOCKET_IFNAME"] = "lo"
@@ -53,13 +27,16 @@ def setup(rank, world_size):
     # initialize the process group
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
+
 def cleanup():
     dist.destroy_process_group()
-#%% md
+
+
+# %% md
 # 2.1 Define our toy model for handwritten digit classification.
 #
 #
-#%%
+# %%
 def prepare_dataset(rank, world_size, args, tokenizer, max_length=512):
     # Load dataset (example with wikitext, replace with your dataset)
     dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
@@ -70,24 +47,21 @@ def prepare_dataset(rank, world_size, args, tokenizer, max_length=512):
             truncation=True,
             max_length=max_length,
             padding="max_length",
-            return_tensors="pt"
+            return_tensors="pt",
         )
         outputs["labels"] = outputs["input_ids"].clone()
         return outputs
 
     tokenized_dataset = dataset.map(
-        tokenize_function,
-        batched=True,
-        remove_columns=dataset["train"].column_names
+        tokenize_function, batched=True, remove_columns=dataset["train"].column_names
     )
 
-    train_sampler = DistributedSampler(tokenized_dataset["train"],
-                                       rank=rank,
-                                       num_replicas=world_size,
-                                       shuffle=True)
-    test_sampler = DistributedSampler(tokenized_dataset["validation"],
-                                      rank=rank,
-                                      num_replicas=world_size)
+    train_sampler = DistributedSampler(
+        tokenized_dataset["train"], rank=rank, num_replicas=world_size, shuffle=True
+    )
+    test_sampler = DistributedSampler(
+        tokenized_dataset["validation"], rank=rank, num_replicas=world_size
+    )
 
     train_loader = torch.utils.data.DataLoader(
         tokenized_dataset["train"],
@@ -96,7 +70,7 @@ def prepare_dataset(rank, world_size, args, tokenizer, max_length=512):
         collate_fn=default_data_collator,
         num_workers=2,
         pin_memory=True,
-        shuffle=False
+        shuffle=False,
     )
 
     test_loader = torch.utils.data.DataLoader(
@@ -106,20 +80,22 @@ def prepare_dataset(rank, world_size, args, tokenizer, max_length=512):
         collate_fn=default_data_collator,
         num_workers=2,
         pin_memory=True,
-        shuffle=False
+        shuffle=False,
     )
 
     return train_loader, test_loader, train_sampler
 
-#%% md
+
+# %% md
 # 2.2 Define a train function
-#%%
+# %%
 def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler=None):
     model.train()
     ddp_loss = torch.zeros(2).to(rank)
     if sampler:
         sampler.set_epoch(epoch)
-    for batch_idx, batch in tqdm(enumerate(train_loader), total=len(train_loader)):
+    pbar = tqdm(enumerate(train_loader), total=len(train_loader), desc="Training")
+    for batch_idx, batch in pbar:
         batch = {k: v.to(rank) for k, v in batch.items()}
         optimizer.zero_grad()
         outputs = model(**batch)
@@ -129,16 +105,18 @@ def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler
 
         ddp_loss[0] += loss.item()
         ddp_loss[1] += len(batch["input_ids"])
+        pbar.set_postfix({"loss": f"{loss.item():.4f}"})
 
     dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
     if rank == 0:
-        print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, ddp_loss[0] / ddp_loss[1]))
+        print("Train Epoch: {} \tLoss: {:.6f}".format(epoch, ddp_loss[0] / ddp_loss[1]))
 
-#%% md
+
+# %% md
 # 2.3 Define a validation function
-# 
-# 
-#%%
+#
+#
+# %%
 def test(model, rank, world_size, test_loader):
     model.eval()
     ddp_loss = torch.zeros(2).to(rank)
@@ -154,66 +132,73 @@ def test(model, rank, world_size, test_loader):
 
     if rank == 0:
         test_loss = ddp_loss[0] / ddp_loss[1]
-        print(f'Test set: Average loss: {test_loss:.4f}\n')
+        print(f"Test set: Average loss: {test_loss:.4f}\n")
 
-#%% md
+
+# %% md
 # 2.4 Define a distributed train function that wraps the model in FSDP
-# 
+#
 # **Note: to save the FSDP model, we need to call the state_dict on each rank then on Rank 0 save the overall states.**
-#%%
+# %%
 def fsdp_main(rank, world_size, args):
     setup(rank, world_size)
 
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
     tokenizer.pad_token = tokenizer.eos_token
 
-    train_loader, test_loader, train_sampler = prepare_dataset(rank, world_size, args, tokenizer)
+    train_loader, test_loader, train_sampler = prepare_dataset(
+        rank, world_size, args, tokenizer
+    )
 
     torch.cuda.set_device(rank)
 
     model = AutoModelForCausalLM.from_pretrained(
         args.model_name,
-        torch_dtype=torch.bfloat16,
-        # device_map="auto"
+        # torch_dtype=torch.bfloat16,
     )
     model = model.to(rank)
 
     # # Wrap model with FSDP
     fsdp_config = {}
-    fsdp_config = dict(
-        limit_all_gathers=True,
-        mixed_precision=MixedPrecision(
-            param_dtype=torch.bfloat16,
-            reduce_dtype=torch.bfloat16,
-            buffer_dtype=torch.bfloat16
-        ),
-        sharding_strategy=ShardingStrategy.FULL_SHARD,
-        device_id=torch.cuda.current_device(),
-    )
+    # fsdp_config = dict(
+    #     limit_all_gathers=True,
+    #     mixed_precision=MixedPrecision(
+    #         param_dtype=torch.bfloat16,
+    #         reduce_dtype=torch.bfloat16,
+    #         buffer_dtype=torch.bfloat16
+    #     ),
+    #     sharding_strategy=ShardingStrategy.FULL_SHARD,
+    #     device_id=torch.cuda.current_device(),
+    # )
 
-    model = FSDP(model, **fsdp_config)# , **fsdp_config
+    model = FSDP(model, **fsdp_config)  # , **fsdp_config
 
-    # Initialize optimizer
-    optimizer = torch.optim.AdamW(
-        model.parameters(),
-        lr=args.lr,
-        weight_decay=0.01
-    )
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=0.01)
 
-    # Training loop
     init_start_event = torch.cuda.Event(enable_timing=True)
     init_end_event = torch.cuda.Event(enable_timing=True)
 
     init_start_event.record()
 
     for epoch in range(1, args.epochs + 1):
-        train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler=train_sampler)
+        train(
+            args,
+            model,
+            rank,
+            world_size,
+            train_loader,
+            optimizer,
+            epoch,
+            sampler=train_sampler,
+        )
         test(model, rank, world_size, test_loader)
 
     init_end_event.record()
 
     if rank == 0:
-        print(f"CUDA event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec")
+        print(
+            f"CUDA event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec"
+        )
 
     if args.save_model:
         dist.barrier()
@@ -222,19 +207,17 @@ def fsdp_main(rank, world_size, args):
             torch.save(states, "llama_finetuned.pt")
 
     cleanup()
-#%%
 
 
-if __name__ == '__main__':
-    config = OmegaConf.load('configs/config.yaml')
+# %%
+
+
+if __name__ == "__main__":
+    config = OmegaConf.load("configs/config.yaml")
 
     torch.manual_seed(config.training.seed)
 
     WORLD_SIZE = torch.cuda.device_count()
-    N_PROC = WORLD_SIZE
-    mp.spawn(fsdp_main,
-        args=(N_PROC, config.training),
-        nprocs=N_PROC,
-        join=True)
-#%%
-
+    N_PROC = 6
+    mp.spawn(fsdp_main, args=(N_PROC, config.training), nprocs=N_PROC, join=True)
+# %%
--- fsdp_playground_default.py
+++ fsdp_playground_default.py
@@ -1,43 +1,28 @@
-#%%
+# %%
 # Based on: https://github.com/pytorch/examples/blob/master/mnist/main.py
-import os
 import functools
+import os
+
 import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-from tqdm import tqdm
-import datetime
-
-
-from torch.optim.lr_scheduler import StepLR
-
-import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data.distributed import DistributedSampler
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp.fully_sharded_data_parallel import (
-    CPUOffload,
-    BackwardPrefetch,
-)
-from torch.distributed.fsdp.wrap import (
-    size_based_auto_wrap_policy,
-    enable_wrap,
-    wrap,
-)
-
 from omegaconf import OmegaConf
-import functools
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data.distributed import DistributedSampler
+from torchvision import datasets, transforms
 
 
-#%% md
+# %% md
 # 1.3 Distributed training setup. As we mentioned FSDP is a type of data parallelism which requires a distributed training environment, so here we use two helper functions to initialize the processes for distributed training and clean up.
-#%%
+# %%
 def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
 
     # os.environ["NCCL_DEBUG"] = "INFO"
     # os.environ["NCCL_SOCKET_IFNAME"] = "lo"
@@ -45,13 +30,16 @@ def setup(rank, world_size):
     # initialize the process group
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
+
 def cleanup():
     dist.destroy_process_group()
-#%% md
+
+
+# %% md
 # 2.1 Define our toy model for handwritten digit classification.
-# 
-# 
-#%%
+#
+#
+# %%
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -77,19 +65,23 @@ class Net(nn.Module):
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
-#%% md
+
+
+# %% md
 # 2.2 Define a train function
-#%%
+# %%
 def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler=None):
     model.train()
     ddp_loss = torch.zeros(2).to(rank)
     if sampler:
         sampler.set_epoch(epoch)
-    for batch_idx, (data, target) in enumerate(train_loader):# , total=len(train_loader)):
+    for batch_idx, (data, target) in enumerate(
+        train_loader
+    ):  # , total=len(train_loader)):
         data, target = data.to(rank), target.to(rank)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target, reduction='sum')
+        loss = F.nll_loss(output, target, reduction="sum")
         loss.backward()
         optimizer.step()
 
@@ -98,12 +90,14 @@ def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler
 
     dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
     if rank == 0:
-        print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, ddp_loss[0] / ddp_loss[1]))
-#%% md
+        print("Train Epoch: {} \tLoss: {:.6f}".format(epoch, ddp_loss[0] / ddp_loss[1]))
+
+
+# %% md
 # 2.3 Define a validation function
-# 
-# 
-#%%
+#
+#
+# %%
 def test(model, rank, world_size, test_loader):
     model.eval()
     correct = 0
@@ -112,8 +106,12 @@ def test(model, rank, world_size, test_loader):
         for data, target in test_loader:
             data, target = data.to(rank), target.to(rank)
             output = model(data)
-            ddp_loss[0] += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            ddp_loss[0] += F.nll_loss(
+                output, target, reduction="sum"
+            ).item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
             ddp_loss[1] += pred.eq(target.view_as(pred)).sum().item()
             ddp_loss[2] += len(data)
 
@@ -121,48 +119,51 @@ def test(model, rank, world_size, test_loader):
 
     if rank == 0:
         test_loss = ddp_loss[0] / ddp_loss[2]
-        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-            test_loss, int(ddp_loss[1]), int(ddp_loss[2]),
-            100. * ddp_loss[1] / ddp_loss[2]))
-#%% md
+        print(
+            "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
+                test_loss,
+                int(ddp_loss[1]),
+                int(ddp_loss[2]),
+                100.0 * ddp_loss[1] / ddp_loss[2],
+            )
+        )
+
+
+# %% md
 # 2.4 Define a distributed train function that wraps the model in FSDP
-# 
+#
 # **Note: to save the FSDP model, we need to call the state_dict on each rank then on Rank 0 save the overall states.**
-#%%
+# %%
 def fsdp_main(rank, world_size, args):
 
     setup(rank, world_size)
 
-    transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    )
 
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
-                        transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
-                        transform=transform)
+    dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
+    dataset2 = datasets.MNIST("../data", train=False, transform=transform)
 
-    sampler1 = DistributedSampler(dataset1, rank=rank, num_replicas=world_size, shuffle=True)
+    sampler1 = DistributedSampler(
+        dataset1, rank=rank, num_replicas=world_size, shuffle=True
+    )
     sampler2 = DistributedSampler(dataset2, rank=rank, num_replicas=world_size)
     # sampler1 = None
     # sampler2 = None
 
-    train_kwargs = {'batch_size': args.batch_size, 'sampler': sampler1}
-    test_kwargs = {'batch_size': args.test_batch_size, 'sampler': sampler2}
-    cuda_kwargs = {'num_workers': 2,
-                    'pin_memory': True,
-                    'shuffle': False}
+    train_kwargs = {"batch_size": args.batch_size, "sampler": sampler1}
+    test_kwargs = {"batch_size": args.test_batch_size, "sampler": sampler2}
+    cuda_kwargs = {"num_workers": 2, "pin_memory": True, "shuffle": False}
     train_kwargs.update(cuda_kwargs)
     test_kwargs.update(cuda_kwargs)
 
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
     my_auto_wrap_policy = functools.partial(
         size_based_auto_wrap_policy, min_num_params=100
     )
     torch.cuda.set_device(rank)
-
 
     init_start_event = torch.cuda.Event(enable_timing=True)
     init_end_event = torch.cuda.Event(enable_timing=True)
@@ -178,14 +179,25 @@ def fsdp_main(rank, world_size, args):
     if rank == 0:
         print("Starting training")
     for epoch in range(1, args.epochs + 1):
-        train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler=sampler1)
+        train(
+            args,
+            model,
+            rank,
+            world_size,
+            train_loader,
+            optimizer,
+            epoch,
+            sampler=sampler1,
+        )
         test(model, rank, world_size, test_loader)
         scheduler.step()
 
     init_end_event.record()
 
     if rank == 0:
-        print(f"CUDA event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec")
+        print(
+            f"CUDA event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec"
+        )
         print(f"{model}")
 
     if args.save_model:
@@ -196,19 +208,17 @@ def fsdp_main(rank, world_size, args):
             torch.save(states, "mnist_cnn.pt")
 
     cleanup()
-#%%
 
 
-if __name__ == '__main__':
-    config = OmegaConf.load('configs/config.yaml')
+# %%
+
+
+if __name__ == "__main__":
+    config = OmegaConf.load("configs/config.yaml")
 
     torch.manual_seed(config.training.seed)
 
     WORLD_SIZE = torch.cuda.device_count()
     N_PROC = 4
-    mp.spawn(fsdp_main,
-        args=(N_PROC, config.training),
-        nprocs=N_PROC,
-        join=True)
-#%%
-
+    mp.spawn(fsdp_main, args=(N_PROC, config.training), nprocs=N_PROC, join=True)
+# %%
